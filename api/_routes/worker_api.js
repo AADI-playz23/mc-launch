0028914e3f610f85319da80698c53624bc6cf5eb
Aadarsh Pandey <aadibabaji12345@gmail.com>
2026-06-11 23:49:29 +0530

    Fix worker_api.js making vm_id optional globally

--- api/_routes/worker_api.js
+++ api/_routes/worker_api.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
 
   const valid = validateBody(req, res, {
     op: { required: true, type: 'string' },
-    vm_id: { required: true, type: 'string' }
+    vm_id: { required: false, type: 'string' }
   });
   if (!valid) return;
 
